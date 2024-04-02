4baad71f9e9611f8681f19d50e77ee29222c2f02
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-02 20:47:39 +0300

    Update and rename scanallp.sh to scanall.sh

--- scanallp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -n  "[+] Enter Target : "
-read Target
-echo "Please wait..."
-nmap -v -p 0-65535 -A $Target > "scanallports_data.txt"
-echo -e "Data is saved as scanallports_data.txt"
