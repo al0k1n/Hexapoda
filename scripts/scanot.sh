e911adaf79c6ec6479b4e31b7ac241a5b48d7b7a
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-03 23:02:24 +0300

    Update scanot.sh

--- scripts/scanot.sh
+++ scripts/scanot.sh
@@ -3,4 +3,4 @@ echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
 nmap -sV -sC -O -T4 -n -Pn $Target > "scanot_data.txt"
-echo -e "Data is saved as scanot_data.txt"
+echo -e "Check subfinder.txt" 
