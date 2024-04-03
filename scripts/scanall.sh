20e993d3a9c8422bb888bcf86f60f6f82593c5cf
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-03 23:02:36 +0300

    Update scanall.sh

--- scripts/scanall.sh
+++ scripts/scanall.sh
@@ -3,4 +3,4 @@ echo -n  "[+] Enter Target : "
 read Target
 echo "Please wait..."
 nmap -v -p 0-65535 -A $Target > "scanall_data.txt"
-echo -e "Data is saved as scanall_data.txt"
+echo -e "Check subfinder.txt"
