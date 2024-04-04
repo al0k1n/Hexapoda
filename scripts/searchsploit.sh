b73ef465cd5e1a853d20adae9f491a0103ba9d2b
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 01:16:53 +0300

    Update searchsploit.sh

--- scripts/searchsploit.sh
+++ scripts/searchsploit.sh
@@ -6,23 +6,10 @@ if ! command -v searchsploit &> /dev/null; then
     exit 1
 fi
 
-# Проверка текущей директории
-if [[ ! $(pwd) =~ "/Hexapoda" ]]; then
-    echo "Error: This script must be run from within the Hexapoda directory."
-    exit 1
-fi
-
 # Проверка наличия поддиректории outputs
 if [ ! -d "outputs" ]; then
     echo "Error: The 'outputs' directory does not exist in the Hexapoda directory."
     echo "Please make sure the 'outputs' directory exists in the Hexapoda directory before running this script."
-    exit 1
-fi
-
-# Проверка наличия поддиректории outputs
-if [ ! -d "scripts" ]; then
-    echo "Error: The 'scripts' directory does not exist in the Hexapoda directory."
-    echo "Please make sure the 'scripts' directory exists in the Hexapoda directory before running this script."
     exit 1
 fi
 
@@ -37,5 +24,4 @@ read -p "Enter the keyword to search for vulnerabilities: " keyword
 # Поиск уязвимостей с использованием searchsploit и сохранение результатов в файл
 searchsploit "$keyword" > "$output_directory/$output_file"
 
-# Вывод дополнительной информации
 echo "Search results have been saved to $output_file."
