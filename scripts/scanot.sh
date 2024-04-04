bc1e39e45d232e30b7d346722567a7ebc076105f
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 01:29:23 +0300

    Update scanot.sh

--- scripts/scanot.sh
+++ scripts/scanot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if ! command -v nmap &> /dev/null; then
+    echo "Error: dirsearch is not installed. Please install it before using this script."
+    exit 1
+fi
+
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
