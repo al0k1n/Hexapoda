7e513be5444b4d3bd10079d1139977cd0ba8c2f0
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 01:43:56 +0300

    Create scanudp100.sh

---
+++ scripts/scanudp100.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if ! command -v nmap &> /dev/null; then
+    echo "Error: dirsearch is not installed. Please install it before using this script."
+    exit 1
+fi
+
+echo -n "[+] Enter Target : "
+read Target
+nmap -sU -sV --version-intensity 0 -n -F -T4 $target > "scanudp100_out.txt"
+echo -e "Data is saved as scanudp100_out.txt"
