d33ac567fb00630febb293a8ba8365f9c7cc4130
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-04 23:59:32 +0300

    Create dirsearch.sh

---
+++ scripts/dirsearch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Вывод сохранён в файл dirsiearch_output.txt, он находится по пути Hexapoda/outputs
+    echo -e "Output is saved to the file dirsearch_output.txt, it is located in the Hexapoda/outputs directory"
+# Проверка наличия утилиты dirsearch
+if ! command -v dirsearch &> /dev/null; then
+    echo "Error: dirsearch is not installed. Please install it before using this script."
+    exit 1
+fi
+
+# Директория для сохранения результатов
+output_directory="outputs"
+
+# Проверка текущей директории
+if [[ ! $(pwd) =~ "/Hexapoda" ]]; then
+    echo "Error: This script must be run from within the Hexapoda directory."
+    exit 1
+fi
+
+# Проверка наличия поддиректории outputs
+if [ ! -d "outputs" ]; then
+    echo "Error: The 'outputs' directory does not exist in the Hexapoda directory."
+    echo "Please make sure the 'outputs' directory exists in the Hexapoda directory before running this script."
+    exit 1
+fi
+
+# Запрос цели для сканирования
+read -p "Enter the target URL (http://example.com): " target_url
+
+# Запуск dirsearch для указанной цели и сохранение результатов в указанную директорию
+dirsearch -u "$target_url" -o "$output_directory/dirsearch_output.txt"
