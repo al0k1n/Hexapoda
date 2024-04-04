128c6d6b469941ff5d0cc040cd2145bbac10a55a
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 01:45:27 +0300

    Update hostdiscovery.sh

--- scripts/hostdiscovery.sh
+++ scripts/hostdiscovery.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if ! command -v netdiscover &> /dev/null; then
+    echo "Error: dirsearch is not installed. Please install it before using this script."
+    exit 1
+fi
+
 echo "Example target: 0.0.0.0/24"
 echo -n "[+] Enter Target : "
 read Target
