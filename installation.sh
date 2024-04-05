cb67f13501efec638080abe795c30b7c5b8bc767
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 15:16:36 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,1 +1,30 @@
 
+function setupEnvironment(){
+    echo "checking your system and setting up the environment: "
+    eval $(echo "$sudoo apt update;")    
+
+    echo "setting gopath";
+    if [ -f ~/.bashrc ]; then
+        echo "export GOPATH=\$HOME/go" >> ~/.bashrc
+        echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
+        source ~/.bashrc;
+    elif [ -f ~/.zshrc ]; then
+        echo "export GOPATH=\$HOME/go" >> ~/.zshrc
+        echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.zshrc
+        source ~/.zshrc;
+    fi
+
+    echo "Installing golang"
+    eval $(echo "$sudoo apt install -y golang;")
+
+    echo "Installing Python3/pip3"
+    eval $(echo "$sudoo apt install -y python3;")
+    eval $(echo "$sudoo apt install -y python3-pip;")
+
+    echo "Installing git"
+    eval $(echo "$sudoo apt install -y git;")
+
+    echo "Installing libcap-dev"
+    eval $(echo "$sudoo apt install -y libpcap-dev;")
+}
+setupEnvironment
