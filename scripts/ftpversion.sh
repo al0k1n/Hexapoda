29aff4a418f6ec476ffd73e0360b7b46f60aab37
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 13:39:52 +0300

    Update ftpversion.sh

--- scripts/ftpversion.sh
+++ scripts/ftpversion.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Проверка наличия ftp
 if ! command -v nmap &> /dev/null; then
-    echo "Error: searchsploit is not installed. Please install it before using this script."
+    echo "Error: nmap is not installed. Please install it before using this script."
     exit 1
 fi
 
