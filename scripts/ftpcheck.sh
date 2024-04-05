1d5b445edc41e1a192ddae630b9dd1ca0c0ce142
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 13:31:23 +0300

    Update ftpcheck.sh

--- scripts/ftpcheck.sh
+++ scripts/ftpcheck.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if ! command -v netcat &> /dev/null; then
-    echo "Error: dirsearch is not installed. Please install it before using this script."
-    exit 1
-fi
-
 echo -e "Data is saved as ftpcheck_out.txt"
 echo "To terminate the process, press 'CTRL+C'."
 echo -n "[+] Enter Target : "
