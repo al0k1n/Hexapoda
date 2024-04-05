b444d79c7f2ebb94d961f24279609bb7f812d608
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 16:06:18 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -13,145 +13,98 @@ echo ""
 
 case $number in
 	1)
-		echo -n "Enter the domain: "
-		read domain
-             	subfinder -d $domain -o output.txt
-		;;
+		cd /scritps
+  		bash script.sh
+    		;;
 
 	2)
-		echo -n "Enter the domain: "
-		read domain
-             	subfinder -d $domain | dnsx -cname -resp
+
 		;;
           
 	3)
-		echo -n "Enter the domain: "
-		read domain
-             	echo "$domain" | nuclei -t nuclei-templates -o $domain.txt
+
 		;;
 	
 	4)
-		echo -n "Enter the domain: "
-		read domain
-  	        subfinder -d $domain | httpx -silent
+
 		;;
 
 	5)
-		echo -n "Enter the domain: "
-		read domain
-             	echo "$domain" | waybackurls > $domain.txt
+
 		;;
 
 	6)
-		echo -n "Enter the domain: "
-		read domain
-             	gau -o $domain.txt $domain
+
 		;;
 
 	7)
-		echo -n "Enter the domain: "
-		read domain
-             	naabu -host $domain
+		
 		;;
 
 	8)
-		echo -n "Enter the domain: "
-		read domain
-             	echo "$domain" | aquatone
+		
 		;;
 
 	9)
-		echo -n "Enter the domain: "
-		read domain
-              	hakrawler -url $domain -depth 1
+		
 		;;
 
 	10)
-		echo -n "Enter the domain: "
-		read domain
-             	ffuf -w /usr/share/wordlists/rockyou.txt -u https://$domain/FUZZ
+		
 		;;
 	11)
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/dirsearch/dirsearch.py -u $domain -e all
+		
 		;;
 	
 	12)
-		echo -n "Enter the domain: "
-		read domain
-		amass intel -d $domain -whois -o output.txt
+		
 		;;
 
 	13)
-		echo -n "Enter the domain: "
-		read domain
-		dalfox url http://$domain -b hahwul.xss.ht
+		
 		;;
 
 	14)
-		echo -n "Enter the domain: "
-		read domain
-		findomain -t $domain -o
+		
 		;;
 
 	15)
-		echo -n "Enter the domain: "
-		read domain
-		arjun -u https://$domain
+		
 		;;
 
 
 	16)
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/LinkFinder/linkfinder.py -i https://$domain -d 
+		
 		;;
 
 	17)
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/Secretfinder/SecretFinder.py -i $domain -o output.html
+		
 		;;
 
 	18)
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/ParamSpider/paramspider.py --domain $domain
+		
 		;;
 
 	19)
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/Corsy/corsy.py -u https://$domain
+		
 		;;
 
 	
 	20)
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/gitGraber/gitGraber.py -k keywordsfile.txt \"$domain\" -s
+		
 		;;
 
 	21)
-		echo -n "Enter the URL: "
-		read  url
-		python3 ~/byp4xx/byp4xx.py $url
-		;;
+
+  		;;
 
         22) 
-                echo -n "Enter your file: "
-		read  file
-		subjack -w $file -t 100 -timeout 30 -o results.txt -ssl
-		;;
 
+	       ;;
 	23)
-		echo "Make sure to close all firefox windows before using this tool";
-		echo -n "Enter the domain: "
-		read domain
-		python3 ~/webscreenshot/webscreenshot.py -v $domain -r firefox
-		;;
-		
+
+ 		;;
+   
 	*)	
 		echo "Please give valid choice!!!"
 		;;
