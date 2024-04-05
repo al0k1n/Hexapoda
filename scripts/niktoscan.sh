40faa358c5a3a3ef319bd19fa7b862cde0af41e8
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 03:42:57 +0300

    Create niktoscan.sh

---
+++ scripts/niktoscan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! command -v nikto &> /dev/null; then
+    echo "Error: dirsearch is not installed. Please install it before using this script."
+    exit 1
+fi
+
+echo -n "[+] Enter Target : "
+read Target
+echo "Please wait..."
+nikto -h $Target > "niktoscan_out.txt"
+echo -e "Data is saved as niktoscan_out.txt"
