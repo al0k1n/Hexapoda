585967177c0ec5ca66237c81853d968464a3454e
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 13:34:46 +0300

    Update sshcheck.sh

--- scripts/sshcheck.sh
+++ scripts/sshcheck.sh
@@ -1,16 +1,4 @@
 !/bin/bash
-# Проверка наличия ftp
-if ! command -v ssh &> /dev/null; then
-    echo "Error: searchsploit is not installed. Please install it before using this script."
-    exit 1
-fi
-
-# Проверка наличия nmap
-if ! command -v netcat &> /dev/null; then
-    echo "Error: searchsploit is not installed. Please install it before using this script."
-    exit 1
-fi
-
 echo -n "[+] Enter Target : "
 read Target
 nc -nv $Target 22 > "sshcheck_out.txt"
