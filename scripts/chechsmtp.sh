ca619213a5007c7edccace3041d4d545835679fe
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 13:29:45 +0300

    Update chechsmtp.sh

--- scripts/chechsmtp.sh
+++ scripts/chechsmtp.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-if ! command -v netcat &> /dev/null; then
-    echo "Error: dirsearch is not installed. Please install it before using this script."
-    exit 1
-fi
-
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
