260b5e2791730245becc0228939ba38a00ab53be
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 03:21:45 +0300

    Create sshlocalusers.sh

---
+++ scripts/sshlocalusers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+   # Проверка наличия searchsploit
+if ! command -v msfconsole &> /dev/null; then
+    echo "Error: searchsploit is not installed. Please install it before using this script."
+    exit 1
+fi
+echo -n "[+] Enter Host: "
+read Host
+echo -n "[+] Enter Port: "
+read Port
+echo "Find users..."
+msfconsole -q -x "use scanner/ssh/ssh_enumusers; set rhosts $Host; set RPORT $Port; set user_file /usr/share/wordlists/metasploit/default_users_for_services_unhash.txt; run; exit" > "sshlocalusers_out.txt"
+echo -e "Data is saved as sshlocalusers_out.txt"
