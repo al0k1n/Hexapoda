8cf2080f05ebed2faf8a6c1b4dd0993fe8087536
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 15:16:02 +0300

    Update Hexapoda.sh

--- Hexapoda.sh
+++ Hexapoda.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/bash
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+. "$scriptDir/logo.sh" # запуск лого
+. "$scriptDir/installation.sh" # запуск установки необходимых компонентов
+
+if [ -f $scriptDir/.installed ]; then
+    log ".installed file found";
+    log "Expecting things to be already present"
+else
+    log "Things might not be installed"
+    log "Will Setup Environment and install assets"
+    setupEnvironment
+    abort_on_failure "Failed to setup Environment"
+    setupAssets  
+    abort_on_failure "Failed to Setup Assets"
+    touch $scriptDir/.installed
+
+fi
+
+. "$scriptDir/launch.sh" # запуск 
