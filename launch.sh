ce83ca2c4d1a1315c988a890305aaee588b6b1ec
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-05 23:44:49 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-echo "█----------------------------------█"
-echo "█	      Scans                █     FTP SSH"
-echo "█----------------------------------█"
-echo "█ 1.  Host disvovery		   █   6. Check FTP vulners"	
-echo "█ 2.  Scan all TCP ports	   █   7. Are you FTP?"
-echo "█ 3.  Scan 1000 popular TCP ports  █ 8. Check FTP version"
-echo "█ 4.  Fast scan 		   █"
-echo "█ 5.  Scan 100 popular UDP ports   █"
-echo "█----------------------------------█"
+cd /scritps
+echo "█----------------------------------█-----------------------█----------------------------█"
+echo "█	      Scans                █          FTP          █            SSH             █"
+echo "█----------------------------------█-----------------------█----------------------------█"
+echo "█ 1. Host disvovery		   █ 6. Check FTP vulners  █ 9. Are you SSH?            █"	
+echo "█ 2. Scan all TCP ports	           █ 7. Are you FTP?       █ 10. Version                █"
+echo "█ 3. Scan 1000 popular TCP ports   █ 8. Check FTP version  █ 11. Fingerprint            █"
+echo "█ 4. Fast scan 		           █                       █ 12. Check Enum Users       █"
+echo "█ 5. Scan 100 popular UDP ports    █                       █                            █"
+echo "█----------------------------------█-----------------------█----------------------------█"
+echo "█               WEB                █          SMB          █          Others            █"
+echo "█----------------------------------█-----------------------█----------------------------█"
+echo "█ 13. Check hidden directories     █ 18. SMB Information   █ 23. BruteForce             █"
+echo "█ 14. Nikto scan                   █ 19. Version           █ 24. Search exploits        █"
+echo "█ 15. Search subdomains            █ 20. Check vulners     █ 25. Audit report           █"
+echo "█ 16. Check page source code       █ 21. EternalBlue       █                            █"
+echo "█ 17. Server header check          █ 22. Shared drives     █                            █"
+echo "█----------------------------------█-----------------------█----------------------------█"
+echo ""
 echo ""
 echo ""
 echo -n "Enter the respective number from tool list that you want to use: "
-
-
-
 read number
 echo ""
-
-
-
-SMB 		WEB	SMTP 		BruteForce		Searchsploit  		Report  
 case $number in
 	1)
-		cd /scritps
   		bash script.sh
     		;;
 
