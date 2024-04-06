722e45fa6385611621d4f0d916ccf5481a808745
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 13:30:53 +0300

    Update scantcpall.sh

--- scantcpall.sh
+++ scantcpall.sh
@@ -4,7 +4,13 @@ if ! command -v nmap &> /dev/null; then
     exit 1
 fi
 
-echo -n "[+] Enter Target : "
+output_directory="outputs"
+output_file="scantcpall_out.txt"
+
+echo -n "[+] Enter Target (0.0.0.0/24) : "
 read Target
-nmap $Target -p- -v -sV --reason > "scantcpall_out.txt"
-echo -e "Data is saved as scantcpall_out.txt"
+echo "Please wait..."
+nmap $Target -p- -v -sV --reason | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
