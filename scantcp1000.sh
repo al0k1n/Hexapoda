b57e69402d67c8cc17923100a1aa8c9f621bfd4b
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 22:40:13 +0300

    Update scantcp1000.sh

--- scantcp1000.sh
+++ scantcp1000.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+
+# Проверка наличия команды nmap
+# Eсли команда nmap не найдена, выводится сообщение об ошибке и
+# скрипт завершает свою работу с кодом выхода 1.
 if ! command -v nmap &> /dev/null; then
     echo "Error: nmap is not installed. Please install it before using this script."
     exit 1
 fi
 
+# Переменная, которая содержит имя директории для сохранения результатов
 output_directory="outputs"
+
+# Переменная, которая содержит имя файла для сохранения результатов
 output_file="scantcp1000_out.txt"
 
 echo -n "[+] Enter Target : "
+
+# Запрос на ввод
 read Target
+
 echo "Please wait..."
+
+# Команда, которая запускает утилиту nmap для сканирования 1000 популярных TCP портов
+# Результат выводится на экран и записывается в директорию outputs с названием scantcp1000_out.txt
 nmap -sV -sC -O -T4 -n -Pn $Target | tee "$output_directory/$output_file"
 echo "                                        "
 echo "Results have been saved to $output_file."
