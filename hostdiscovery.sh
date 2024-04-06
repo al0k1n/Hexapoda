e3c48963ba878551994ac360d371c32f28b55db6
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 16:09:58 +0300

    Update hostdiscovery.sh

--- hostdiscovery.sh
+++ hostdiscovery.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Проверка наличия команды netdiscover
+# Eсли команда netdiscover не найдена, выводится сообщение об ошибке и 
+# скрипт завершает свою работу с кодом выхода 1.
 if ! command -v netdiscover &> /dev/null; then
     echo "Error: netdiscover is not installed. Please install it before using this script."
     exit 1
 fi
 
+# Переменная, которая содержит имя директории для сохранения результатов
 output_directory="outputs"
+
+# Переменная, которая содержит имя файла для сохранения результатов
 output_file="hostdiscovery_out.txt"
 
 echo "Example target: 0.0.0.0/24"
 echo -n "[+] Enter Target : "
+# Запрос на ввод 
 read Target
 echo "Please wait..."
+
+# Команда, которая запускает утилиту netdiscover для поиска хостов в заданной сети
+# Результат выводится на экран и записывается в директорию outputs с названием hostdiscovery_out.txt
+
 netdiscover -r $Target | tee "$output_directory/$output_file"
 echo "                                        "
 echo "Results have been saved to $output_file."
