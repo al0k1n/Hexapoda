f2a658d80a183f0b26e67a29389b3f656816cad3
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:52:09 +0300

    Update hostdiscovery.sh

--- hostdiscovery.sh
+++ hostdiscovery.sh
@@ -5,8 +5,14 @@ if ! command -v netdiscover &> /dev/null; then
     exit 1
 fi
 
+output_directory="outputs"
+output_file="hostdiscovery_out.txt"
+
 echo "Example target: 0.0.0.0/24"
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
-netdiscover -r $Target
+netdiscover -r $Target | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
