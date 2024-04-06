c92be95e522398855e489d62028c8008d767e8a5
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 13:06:55 +0300

    Update sourcecodecheck.sh

--- sourcecodecheck.sh
+++ sourcecodecheck.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+output_directory="outputs"
+output_file="sourcecodecheck_out.txt"
+
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
-curl -i -L $Target > "sourcecodecheck_out.txt"
-echo -e "Data is saved as sourcecodecheck_out.txt"
+curl -i -L $Target | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
