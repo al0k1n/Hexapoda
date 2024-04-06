f1d129860d0a02f25ee8d50b37a92da8c1af921f
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 13:26:40 +0300

    Update subfinder.sh

--- subfinder.sh
+++ subfinder.sh
@@ -4,8 +4,13 @@ if ! command -v subfinder &> /dev/null; then
     exit 1
 fi
 
-echo -n "Enter the domain: " 
+output_directory="outputs"
+output_file="subfinder._out.txt"
+
+echo -n "Enter the domain (example.com): " 
 read domain	
 echo "Starting..." 
-subfinder -d $domain -o subfinder_out.txt
-echo "Check subfinder_out.txt" 
+subfinder -d $domain -o "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
