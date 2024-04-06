8b6d03770a5b247d56f62ee22c21af8d4bc78d0c
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 22:42:39 +0300

    Update scantcpall.sh

--- scantcpall.sh
+++ scantcpall.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+
+# Проверка наличия команды nmap
+# Eсли команда nmap не найдена, выводится сообщение об ошибке и
+# скрипт завершает свою работу с кодом выхода 1.
 if ! command -v nmap &> /dev/null; then
     echo "Error: nmap is not installed. Please install it before using this script."
     exit 1
 fi
 
+# Переменная, которая содержит имя директории для сохранения результатов
 output_directory="outputs"
+
+# Переменная, которая содержит имя файла для сохранения результатов
 output_file="scantcpall_out.txt"
 
 echo -n "[+] Enter Target (0.0.0.0/24) : "
+
+# Запрос на ввод
 read Target
 echo "Please wait..."
+
+# Команда выполняет сканирование всех TCP портов на целевом хосте
 nmap $Target -p- -v -sV --reason | tee "$output_directory/$output_file"
 echo "                                        "
 echo "Results have been saved to $output_file."
