eba36822740764a13a58c1b13e4bdc42c4aca6a0
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:49:47 +0300

    Update headcheck.sh

--- headcheck.sh
+++ headcheck.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+output_directory="outputs"
+output_file="headcheck_out.txt"
+
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
-curl -I $Target > "headcheck_out.txt"
-echo -e "Data is saved as headcheck_out.txt"
+curl -I $Target | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
