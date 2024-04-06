0d8f520c1a42b5d7cc64f709f88045f441186b95
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:54:07 +0300

    Update niktoscan.sh

--- niktoscan.sh
+++ niktoscan.sh
@@ -4,8 +4,13 @@ if ! command -v nikto &> /dev/null; then
     exit 1
 fi
 
+output_directory="outputs"
+output_file="niktoscan_out.txt"
+
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
-nikto -h $Target > "niktoscan_out.txt"
-echo -e "Data is saved as niktoscan_out.txt"
+nikto -h $Target | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
