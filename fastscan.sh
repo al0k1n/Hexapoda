2fced5404cc132d8f452c4682d028d999a5544de
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:27:55 +0300

    Update fastscan.sh

--- fastscan.sh
+++ fastscan.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
 if ! command -v nmap &> /dev/null; then
     echo "Error: nmap is not installed. Please install it before using this script."
     exit 1
 fi
 
+output_directory="outputs"
+output_file="fastscan_out.txt"
+
 echo -n "[+] Enter Target : "
 read Target
 echo "Please wait..."
-nmap -F $Target > "fastscan_out.txt"
-echo -e "Check fastscan_out.txt" 
+nmap -F $Target | tee "$output_directory/$output_file"
+echo "Results have been saved to $output_file."
