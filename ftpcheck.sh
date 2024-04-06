87c575377de8604d4c586d0634cfdc5e9d059711
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:37:10 +0300

    Update ftpcheck.sh

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo -e "Data is saved as ftpcheck_out.txt"
-echo "To terminate the process, press 'CTRL+C'."
+echo "After result, to terminate the process, press 'CTRL+C'."
 echo -n "[+] Enter Target : "
 read Target
-nc -nv $Target 21 > "ftpcheck_out.txt"
+nc -nv $Target 21
