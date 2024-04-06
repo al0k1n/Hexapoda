efa78f0b7b79b8d47202b6512b655033164675b8
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 12:04:45 +0300

    Update checkvulnsmb.sh

--- checkvulnsmb.sh
+++ checkvulnsmb.sh
@@ -4,7 +4,10 @@ if ! command -v nmap &> /dev/null; then
     exit 1
 fi
 
+output_directory="outputs"
+output_file="checkvulnsmb_out.txt"
+
 echo -n "[+] Enter Target : "
 read Target
-nmap --script smb-vuln* -p139,445 -T4 -Pn $Target | tee "checkvulnsmb_out.txt"
+nmap --script smb-vuln* -p139,445 -T4 -Pn $Target | tee "$output_directory/$output_file"
 echo -e "Data is saved as checkvulnsmb_out.txt"
