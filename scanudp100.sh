c74236e5bb8515114f78d756dc768643b165ae07
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-06 13:32:57 +0300

    Update scanudp100.sh

--- scanudp100.sh
+++ scanudp100.sh
@@ -4,7 +4,13 @@ if ! command -v nmap &> /dev/null; then
     exit 1
 fi
 
+output_directory="outputs"
+output_file="scanudp100_out.txt"
+
 echo -n "[+] Enter Target : "
 read Target
-nmap -sU -sV --version-intensity 0 -n -F -T4 $target > "scanudp100_out.txt"
-echo -e "Data is saved as scanudp100_out.txt"
+echo "Please wait..."
+nmap -sU -sV --version-intensity 0 -n -F -T4 $target | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
