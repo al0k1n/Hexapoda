b635d054475e4217f0679458242c23847d428e58
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-07 12:05:30 +0300

    Update sshversion.sh

--- sshversion.sh
+++ sshversion.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Проверка наличия команды msfconsole
+# Eсли команда msfconsole не найдена, выводится сообщение об ошибке и
+# скрипт завершает свою работу с кодом выхода 1.
 if ! command -v msfconsole &> /dev/null; then
     echo "Error: msfconsole is not installed. Please install it before using this script."
     exit 1
@@ -7,11 +11,11 @@ fi
 output_directory="outputs"
 output_file="sshversion_out.txt"
 
-# Функция для использования Metasploit для получения информации о SSH
-    echo -n "Enter Target: "
-    read Target
-    echo "Exploiting SSH..."
-    msfconsole -q -x "use auxiliary/scanner/ssh/ssh_version; set RHOSTS $Target;  run; exit" | tee "$output_directory/$output_file"
-    echo "                                        "
-    echo "Results have been saved to $output_file."
-    echo "                                        "
+# Команда для использования Metasploit для получения информации о SSH
+echo -n "Enter Target: "
+read Target
+echo "Exploiting SSH..."
+msfconsole -q -x "use auxiliary/scanner/ssh/ssh_version; set RHOSTS $Target;  run; exit" | tee "$output_directory/$output_file"
+echo "                                        "
+echo "Results have been saved to $output_file."
+echo "                                        "
