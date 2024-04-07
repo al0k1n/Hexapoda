1e3c69692c0a93ff1084a8052b2abb0e274abde8
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-07 11:40:18 +0300

    Update ftpcheck.sh

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "After result, to terminate the process, press 'CTRL+C'."
 echo -n "[+] Enter Target : "
+
+# Запрос на ввод 
 read Target
+
+# Команда использует nc (netcat) для установки TCP-соединения 
+# с указанным целевым хостом ($Target) на порт 21
 nc -nv $Target 21
