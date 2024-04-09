e6567979098592ddc306e309aaf6c6f9925e4f54
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-09 17:52:40 +0300

    Update logomenu.sh

--- logomenu.sh
+++ logomenu.sh
@@ -1,26 +1,37 @@
-echo "                                                                                                                             dddddddd                  "
-echo "HHHHHHHHH     HHHHHHHHH                                                                                                      d::::::d                  "
-echo "H:::::::H     H:::::::H                                                                                                      d::::::d                  "
-echo "H:::::::H     H:::::::H                                                                                                      d::::::d                  "
-echo "HH::::::H     H::::::HH                                                                                                      d:::::d                   "
-echo "  H:::::H     H:::::H      eeeeeeeeeeee  xxxxxxx      xxxxxxxaaaaaaaaaaaaa  ppppp   ppppppppp      ooooooooooo       ddddddddd:::::d   aaaaaaaaaaaaa   "
-echo "  H:::::H     H:::::H    ee::::::::::::ee x:::::x    x:::::x a::::::::::::a p::::ppp:::::::::p   oo:::::::::::oo   dd::::::::::::::d   a::::::::::::a  "
-echo "  H::::::HHHHH::::::H   e::::::eeeee:::::eex:::::x  x:::::x  aaaaaaaaa:::::ap:::::::::::::::::p o:::::::::::::::o d::::::::::::::::d   aaaaaaaaa:::::a "
-echo "  H:::::::::::::::::H  e::::::e     e:::::e x:::::xx:::::x            a::::app::::::ppppp::::::po:::::ooooo:::::od:::::::ddddd:::::d            a::::a "
-echo "  H:::::::::::::::::H  e:::::::eeeee::::::e  x::::::::::x      aaaaaaa:::::a p:::::p     p:::::po::::o     o::::od::::::d    d:::::d     aaaaaaa:::::a "
-echo "  H::::::HHHHH::::::H  e:::::::::::::::::e    x::::::::x     aa::::::::::::a p:::::p     p:::::po::::o     o::::od:::::d     d:::::d   aa::::::::::::a "
-echo "  H:::::H     H:::::H  e::::::eeeeeeeeeee     x::::::::x    a::::aaaa::::::a p:::::p     p:::::po::::o     o::::od:::::d     d:::::d  a::::aaaa::::::a "
-echo "  H:::::H     H:::::H  e:::::::e             x::::::::::x  a::::a    a:::::a p:::::p    p::::::po::::o     o::::od:::::d     d:::::d a::::a    a:::::a "
-echo "HH::::::H     H::::::HHe::::::::e           x:::::xx:::::x a::::a    a:::::a p:::::ppppp:::::::po:::::ooooo:::::od::::::ddddd::::::dda::::a    a:::::a "
-echo "H:::::::H     H:::::::H e::::::::eeeeeeee  x:::::x  x:::::xa:::::aaaa::::::a p::::::::::::::::p o:::::::::::::::o d:::::::::::::::::da:::::aaaa::::::a "
-echo "H:::::::H     H:::::::H  ee:::::::::::::e x:::::x    x:::::xa::::::::::aa:::ap::::::::::::::pp   oo:::::::::::oo   d:::::::::ddd::::d a::::::::::aa:::a"
-echo "HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeeexxxxxxx      xxxxxxxaaaaaaaaaa  aaaap::::::pppppppp       ooooooooooo      ddddddddd   ddddd  aaaaaaaaaa  aaaa"
-echo "                                                                             p:::::p                                                                   "
-echo "                                                                             p:::::p                                                                   "
-echo "                                                                            p:::::::p                                                                  "
-echo "                                                                            p:::::::p                                                                  "
-echo "                                                                            p:::::::p                                                                  "
-echo "                                                                            ppppppppp                                                                  "
-echo "                                                                                                                                                       "
-echo "                                         "Hexapoda" software package for internal penetration testing  Version 1.0                                     "
-echo "                                                             [Coded By: Nikola Miloshevich]                                                            "
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DEF='\033[0;39m'       #  ${DEF}
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
+echo -e "                                                                                                                             ${WHITE}dddddddd                  "
+echo -e "${WHITE}HHHHHHHHH     HHHHHHHHH                                                                                                      d::::::d                  "
+echo -e "${WHITE}H:::::::H     H:::::::H                                                                                                      d::::::d                  "
+echo -e "${WHITE}H:::::::H     H:::::::H                                                                                                      d::::::d                  "
+echo -e "${WHITE}HH::::::H     H::::::HH                                                                                                      d:::::d                   "
+echo -e "  ${WHITE}H:::::H     H:::::H      eeeeeeeeeeee  xxxxxxx      xxxxxxxaaaaaaaaaaaaa  ppppp   ppppppppp      ooooooooooo       ddddddddd:::::d   aaaaaaaaaaaaa   "
+echo -e "  ${WHITE}H:::::H     H:::::H    ee::::::::::::ee x:::::x    x:::::x a::::::::::::a p::::ppp:::::::::p   oo:::::::::::oo   dd::::::::::::::d   a::::::::::::a  "
+echo -e "  ${WHITE}H::::::HHHHH::::::H   e::::::eeeee:::::eex:::::x  x:::::x  aaaaaaaaa:::::ap:::::::::::::::::p o:::::::::::::::o d::::::::::::::::d   aaaaaaaaa:::::a "
+echo -e "  ${LBLUE}H:::::::::::::::::H  e::::::e     e:::::e x:::::xx:::::x            a::::app::::::ppppp::::::po:::::ooooo:::::od:::::::ddddd:::::d            a::::a "
+echo -e "  ${LBLUE}H:::::::::::::::::H  e:::::::eeeee::::::e  x::::::::::x      aaaaaaa:::::a p:::::p     p:::::po::::o     o::::od::::::d    d:::::d     aaaaaaa:::::a "
+echo -e "  ${LBLUE}H::::::HHHHH::::::H  e:::::::::::::::::e    x::::::::x     aa::::::::::::a p:::::p     p:::::po::::o     o::::od:::::d     d:::::d   aa::::::::::::a "
+echo -e "  ${LBLUE}H:::::H     H:::::H  e::::::eeeeeeeeeee     x::::::::x    a::::aaaa::::::a p:::::p     p:::::po::::o     o::::od:::::d     d:::::d  a::::aaaa::::::a "
+echo -e "  ${LBLUE}H:::::H     H:::::H  e:::::::e             x::::::::::x  a::::a    a:::::a p:::::p    p::::::po::::o     o::::od:::::d     d:::::d a::::a    a:::::a "
+echo -e "${LRED}HH::::::H     H::::::HHe::::::::e           x:::::xx:::::x a::::a    a:::::a p:::::ppppp:::::::po:::::ooooo:::::od::::::ddddd::::::dda::::a    a:::::a "
+echo -e "${LRED}H:::::::H     H:::::::H e::::::::eeeeeeee  x:::::x  x:::::xa:::::aaaa::::::a p::::::::::::::::p o:::::::::::::::o d:::::::::::::::::da:::::aaaa::::::a "
+echo -e "${LRED}H:::::::H     H:::::::H  ee:::::::::::::e x:::::x    x:::::xa::::::::::aa:::ap::::::::::::::pp   oo:::::::::::oo   d:::::::::ddd::::d a::::::::::aa:::a"
+echo -e "${LRED}HHHHHHHHH     HHHHHHHHH    eeeeeeeeeeeeeexxxxxxx      xxxxxxxaaaaaaaaaa  aaaap::::::pppppppp       ooooooooooo      ddddddddd   ddddd  aaaaaaaaaa  aaaa"
+echo -e "                                                                             ${LRED}p:::::p                                                                   "
+echo -e "                                                                             ${LRED}p:::::p                                                                   "
+echo -e "                                                                            ${LRED}p:::::::p                                                                  "
+echo -e "                                                                            ${LRED}p:::::::p                                                                  "
+echo -e "                                                                            ${LRED}p:::::::p                                                                  "
+echo -e "                                                                            ${LRED}ppppppppp                                                                  "
+echo -e "                                                                                                                                                       "
+echo -e "                                         ${LCYAN}"Hexapoda" software package for internal penetration testing  Version 1.0                                     "
+echo -e "                                                             ${LCYAN}[Coded By: Nikola Miloshevich]                                                            "
