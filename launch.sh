15ed77e5806b26d153cc648e66724295907673c3
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-09 17:43:37 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
-#scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#. "$scriptDir/logomenu.sh"
-echo "█----------------------------------█-----------------------█----------------------------█"
-echo "█	      Scans                █          FTP          █            SSH             █"
-echo "█----------------------------------█-----------------------█----------------------------█"
-echo "█ 1. Host disvovery		   █ 6. Check FTP vulners  █ 9.  Are you SSH?           █"	
-echo "█ 2. Scan all TCP ports	           █ 7. Are you FTP?       █ 10. Version                █"
-echo "█ 3. Scan 1000 popular TCP ports   █ 8. Check FTP version  █ 11. Fingerprint            █"
-echo "█ 4. Fast scan 		           █                       █ 12. Check Enum Users       █"
-echo "█ 5. Scan 100 popular UDP ports    █                       █                            █"
-echo "█----------------------------------█-----------------------█----------------------------█"
-echo "█               WEB                █          SMB          █          Others            █"
-echo "█----------------------------------█-----------------------█----------------------------█"
-echo "█ 13. Check hidden directories     █ 18. SMB Information   █ 23. BruteForce             █"
-echo "█ 14. Nikto scan                   █ 19. Version           █ 24. Search exploits        █"
-echo "█ 15. Search subdomains            █ 20. Check vulners     █ 25. Audit report           █"
-echo "█ 16. Check page source code       █ 21. EternalBlue       █                            █"
-echo "█ 17. Server header check          █ 22. Shared drives     █                            █"
-echo "█----------------------------------█-----------------------█----------------------------█"
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DEF='\033[0;39m'       #  ${DEF}
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
+BGBLACK='\033[40m'     #  ${BGBLACK}
+BGRED='\033[41m'       #  ${BGRED}
+BGGREEN='\033[42m'     #  ${BGGREEN}
+BGBROWN='\033[43m'     #  ${BGBROWN}
+BGBLUE='\033[44m'     #  ${BGBLUE}
+BGMAGENTA='\033[45m'     #  ${BGMAGENTA}
+BGCYAN='\033[46m'     #  ${BGCYAN}
+BGGRAY='\033[47m'     #  ${BGGRAY}
+BGDEF='\033[49m'      #  ${BGDEF}
+
+echo -e "█----------------------------------█-------------------------█--------------------------█"
+echo -e "█	      ${WHITE}Scans${NORMAL}                █          ${WHITE}FTP${NORMAL}            █            ${WHITE}SSH${NORMAL}           █"
+echo -e "█----------------------------------█-------------------------█--------------------------█"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}1${NORMAL}${LRED}]${NORMAL} ${WHITE}Host disvovery${NORMAL}		   █ ${LRED}[${NORMAL}${LGREEN}6${NORMAL}${LRED}]${NORMAL} ${WHITE}Check FTP vulners${NORMAL}   █ ${LRED}[${NORMAL}${LGREEN}9${NORMAL}${LRED}]${NORMAL}  ${WHITE}Are you SSH?${NORMAL}        █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}2${NORMAL}${LRED}]${NORMAL} ${WHITE}Scan all TCP ports${NORMAL}	   █ ${LRED}[${NORMAL}${LGREEN}7${NORMAL}${LRED}]${NORMAL} ${WHITE}Are you FTP?${NORMAL}        █ ${LRED}[${NORMAL}${LGREEN}10${NORMAL}${LRED}]${NORMAL} ${WHITE}Version${NORMAL}             █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}3${NORMAL}${LRED}]${NORMAL} ${WHITE}Scan 1000 popular TCP ports${NORMAL}  █ ${LRED}[${NORMAL}${LGREEN}8${NORMAL}${LRED}]${NORMAL} ${WHITE}Check FTP version${NORMAL}   █ ${LRED}[${NORMAL}${LGREEN}11${NORMAL}${LRED}]${NORMAL} ${WHITE}Fingerprint${NORMAL}         █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}4${NORMAL}${LRED}]${NORMAL} ${WHITE}Fast scan${NORMAL}		           █                         █ ${LRED}[${NORMAL}${LGREEN}12${NORMAL}${LRED}]${NORMAL} ${WHITE}Check Enum Users${NORMAL}    █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}5${NORMAL}${LRED}]${NORMAL} ${WHITE}Scan 100 popular UDP ports${NORMAL}   █                         █                          █"
+echo -e "█----------------------------------█-------------------------█--------------------------█"
+echo -e "█               ${WHITE}WEB${NORMAL}                █          ${WHITE}SMB${NORMAL}            █          ${WHITE}Others${NORMAL}          █"
+echo -e "█----------------------------------█-------------------------█--------------------------█"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}13${NORMAL}${LRED}]${NORMAL} ${WHITE}Check hidden directories${NORMAL}    █ ${LRED}[${NORMAL}${LGREEN}18${NORMAL}${LRED}]${NORMAL} ${WHITE}SMB Information${NORMAL}    █ ${LRED}[${NORMAL}${LGREEN}23${NORMAL}${LRED}]${NORMAL} ${WHITE}BruteForce${NORMAL}          █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}14${NORMAL}${LRED}]${NORMAL} ${WHITE}Nikto scan${NORMAL}                  █ ${LRED}[${NORMAL}${LGREEN}19${NORMAL}${LRED}]${NORMAL} ${WHITE}Version${NORMAL}            █ ${LRED}[${NORMAL}${LGREEN}24${NORMAL}${LRED}]${NORMAL} ${WHITE}Search exploits${NORMAL}     █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}15${NORMAL}${LRED}]${NORMAL} ${WHITE}Search subdomains${NORMAL}           █ ${LRED}[${NORMAL}${LGREEN}20${NORMAL}${LRED}]${NORMAL} ${WHITE}Check vulners${NORMAL}      █ ${LRED}[${NORMAL}${LGREEN}25${NORMAL}${LRED}]${NORMAL} ${WHITE}Audit report${NORMAL}        █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}16${NORMAL}${LRED}]${NORMAL} ${WHITE}Check page source code${NORMAL}      █ ${LRED}[${NORMAL}${LGREEN}21${NORMAL}${LRED}]${NORMAL} ${WHITE}EternalBlue${NORMAL}        █                          █"
+echo -e "█ ${LRED}[${NORMAL}${LGREEN}17${NORMAL}${LRED}]${NORMAL} ${WHITE}Server header check${NORMAL}         █ ${LRED}[${NORMAL}${LGREEN}22${NORMAL}${LRED}]${NORMAL} ${WHITE}Shared drives${NORMAL}      █                          █"
+echo -e "█----------------------------------█-------------------------█--------------------------█"
 echo ""
 echo ""
 echo ""
-echo -n "Enter the respective number from tool list that you want to use: "
+echo -e "Enter the respective number from tool list that you want to use: "
 read number
 echo ""
 case $number in
