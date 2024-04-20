3237fbd47064d0df0f485c3bec8175fe23b301cc
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 02:51:34 +0300

    Update hostdiscovery.sh

--- hostdiscovery.sh
+++ hostdiscovery.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DEF='\033[0;39m'       #  ${DEF}
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+green='\e[1;32m' # ${green}
 # Проверка наличия команды netdiscover
 # Eсли команда netdiscover не найдена, выводится сообщение об ошибке и
 # скрипт завершает свою работу с кодом выхода 1.
@@ -14,16 +24,16 @@ output_directory="outputs"
 # Переменная, которая содержит имя файла для сохранения результатов
 output_file="hostdiscovery_out.txt"
 
-echo "Example target: 0.0.0.0/24"
-echo -n "[+] Enter Target : "
+echo -e "Example target: 0.0.0.0/24"
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL $WHITE Enter Target:$NORMAL "
 # Запрос на ввод
 read Target
-echo "Please wait..."
+echo -e "$LYELLOW Please wait...$NORMAL"
 
 # Команда, которая запускает утилиту netdiscover для поиска хостов в заданной сети
 # Результат выводится на экран и записывается в директорию outputs с названием hostdiscovery_out.txt
 
 netdiscover -r $Target | tee "$output_directory/$output_file"
 echo "                                        "
-echo "Results have been saved to $output_file."
+echo -e "$LGREEN Results have been saved to $output_file. $NORMAL"
 echo "                                        "
