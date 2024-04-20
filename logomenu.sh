d4c7f7b3a7de92184bc519ea5fc908167345d3e0
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 02:10:54 +0300

    Update logomenu.sh

--- logomenu.sh
+++ logomenu.sh
@@ -26,7 +26,7 @@ echo -e "${WHITE}H:::::::H     H:::::::H                                        
 echo -e "${WHITE}HH::::::H     H::::::HH                                                                                                      ${LRED}d:::::d                   "
 echo -e "  ${WHITE}H:::::H     H:::::H      eeeeeeeeeeee  xxxxxxx      xxxxxxxaaaaaaaaaaaaa$NORMAL  ${LRED}ppppppppppppppppp      ooooooooooo       ddddddddd:::::d   aaaaaaaaaaaaa   "
 echo -e "  ${WHITE}H:::::H     H:::::H    ee::::::::::::ee x:::::x    x:::::x a::::::::::::a$NORMAL ${LRED}p::::ppp:::::::::p   oo:::::::::::oo   dd::::::::::::::d   a::::::::::::a  "
-echo -e "  ${WHITE}H::::::HHHHH::::::H   e::::::eeeee:::::eex:::::x  x:::::x  aaaaaaaaa:::::a$NORMAL${LRED} p:::::::::::::::::p o:::::::::::::::o d::::::::::::::::d   aaaaaaaaa:::::a "
+echo -e "  ${WHITE}H::::::HHHHH::::::H   e::::::eeeee:::::eex:::::x  x:::::x  aaaaaaaaa:::::a$NORMAL${LRED}p:::::::::::::::::p o:::::::::::::::o d::::::::::::::::d   aaaaaaaaa:::::a "
 echo -e "  ${LBLUE}H:::::::::::::::::H  e::::::e     e:::::e x:::::xx:::::x            a::::a p::::::ppppp::::::po:::::ooooo:::::od:::::::ddddd:::::d            a::::a "
 echo -e "  ${LBLUE}H:::::::::::::::::H  e:::::::eeeee::::::e  x::::::::::x      aaaaaaa:::::a p:::::p     p:::::po::::o     o::::od::::::d    d:::::d     aaaaaaa:::::a "
 echo -e "  ${LBLUE}H::::::HHHHH::::::H  e:::::::::::::::::e    x::::::::x     aa::::::::::::a p:::::p     p:::::po::::o     o::::od:::::d     d:::::d   aa::::::::::::a "
