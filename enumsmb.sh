b978508b7adde2c0f41a09422b94aaa041261278
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 04:05:22 +0300

    Update enumsmb.sh

--- enumsmb.sh
+++ enumsmb.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
 if ! command -v enum4linux &> /dev/null; then
-    echo "Error: enum4linux is not installed. Please install it before using this script."
+    echo "$LRED Error: enum4linux is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
 output_directory="outputs"
 output_file="enumsmb_out.txt"
 
-echo -n "[+] Enter Target : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 read Target
-echo "Please wait..."
+echo "$LYELLOW Please wait...$NORMAL"
 enum4linux $Target | tee "$output_directory/$output_file"
 echo "                                      "
-echo -e "Results is saved as enumsmb_out.txt"
+echo -e "$LGREEN Results is saved as enumsmb_out.txt$NORMAL"
 echo "                                      "
