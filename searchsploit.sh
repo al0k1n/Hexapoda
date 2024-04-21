c700b35424a1bc505d182bc5a9b20ae39959f532
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 03:40:37 +0300

    Update searchsploit.sh

--- searchsploit.sh
+++ searchsploit.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
+
 # Проверка наличия searchsploit
 if ! command -v searchsploit &> /dev/null; then
-    echo "Error: searchsploit is not installed. Please install it before using this script."
+    echo "$LRED Error: searchsploit is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
 # Проверка наличия поддиректории outputs
 if [ ! -d "outputs" ]; then
-    echo "Error: The 'outputs' directory does not exist in the Hexapoda directory."
-    echo "Please make sure the 'outputs' directory exists in the Hexapoda directory before running this script."
+    echo "$LRED Error: The 'outputs' directory does not exist in the Hexapoda directory.$NORMAL"
+    echo "$LRED Please make sure the 'outputs' directory exists in the Hexapoda directory before running this script.$NORMAL"
     exit 1
 fi
 
@@ -17,10 +28,10 @@ output_directory="outputs"
 # Имя файла для сохранения результатов
 output_file="searchsploit_out.txt"
 
-echo "Never limit yourself to searchsploit. Also, search in Google for 'program_name version exploit'."
-read -p "Enter the keyword to search for vulnerabilities: " keyword
+echo "$LGREEN Never limit yourself to searchsploit. Also, search in Google for 'program_name version exploit'.$NORMAL"
+read -pe "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the keyword to search for vulnerabilities: $NORMAL" keyword
 
 # Поиск уязвимостей с использованием searchsploit и сохранение результатов в файл
 searchsploit "$keyword" | tee "$output_directory/$output_file"
 
-echo "Search results have been saved to $output_file."
+echo "$LGREEN Search results have been saved to $output_file.$NORMAL"
