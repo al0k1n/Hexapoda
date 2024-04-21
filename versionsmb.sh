d7c848a60db7b1b13443364fd5427e3089c89c71
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 04:04:22 +0300

    Update versionsmb.sh

--- versionsmb.sh
+++ versionsmb.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
+
 if ! command -v msfconsole &> /dev/null; then
-    echo "Error: msfconsole is not installed. Please install it before using this script."
+    echo -e "$LRED Error: msfconsole is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
-echo -n "[+] Enter Target : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 read Target
 msfconsole -q -x "use scanner/smb/smb_version; set RHOSTS $Target; run; exit" | tee "versionsmb_out.txt"
-echo -e "Data is saved as versionsmb_out.txt"
+echo -e "$LGREEN Data is saved as versionsmb_out.txt$NORMAL"
