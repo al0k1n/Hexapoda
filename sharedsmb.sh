0845f72d98df9501bc988c14aa1a7037a8db3e32
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 04:07:10 +0300

    Update sharedsmb.sh

--- sharedsmb.sh
+++ sharedsmb.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
 output_directory="outputs"
 output_file="sharedsmb_out.txt"
 
-echo -n "[+] Enter Username : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Username:$NORMAL "
 read User
-echo -n "[+] Enter Password : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Password:$NORMAL "
 read Password
-echo -n "[+] Enter Domain (default workgroup): "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Domain (default workgroup):$NORMAL "
 read Domain
-echo -n "[+] Enter Target: "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 read Target
-echo "Please wait..."
+echo "$LYELLOW Please wait...$NORMAL"
 smbmap -u $User -p $Password -d $Domain -H $Target | tee "$output_directory/$output_file"
 echo "                                        "
-echo "Results have been saved to $output_file."
+echo -e "$LGREEN Results have been saved to $output_file.$NORMAL"
 echo "                                        "
