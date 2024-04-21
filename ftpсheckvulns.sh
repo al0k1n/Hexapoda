f01bdd0e0b3537bfd790d3cc4e245e030f5bdaea
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 03:22:41 +0300

    Update ftpсheckvulns.sh

--- ftpсheckvulns.sh
+++ ftpсheckvulns.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
 
 # Проверка наличия команды netdiscover
 # Eсли команда netdiscover не найдена, выводится сообщение об ошибке и 
 # скрипт завершает свою работу с кодом выхода 1.
 if ! command -v nmap &> /dev/null; then
-    echo "Error: nmap is not installed. Please install it before using this script."
+    echo "$LRED Error: nmap is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
@@ -14,7 +23,7 @@ output_directory="outputs"
 # Переменная, которая содержит имя файла для сохранения результатов
 output_file="ftpvulns_out.txt"
 
-echo -n "[+] Enter Target : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 
 # Запрос на ввод
 read Target
@@ -23,5 +32,5 @@ read Target
 # Тем самым произойдёт проверка на наличие уязвимостей FTP на целевом хосте.
 nmap --script ftp-* -p21 $Target | tee "$output_directory/$output_file"
 echo "                                        "
-echo "Results have been saved to $output_file."
+echo "$LGREEN Results have been saved to $output_file.$NORMAL"
 echo "                                        "
