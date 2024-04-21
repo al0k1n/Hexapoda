f7fbc83e040bbfa16ad36f5772bbe3552215d27b
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 03:21:51 +0300

    Update ftpcheck.sh

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "After result, to terminate the process, press 'CTRL+C'."
-echo -n "[+] Enter Target : "
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
+echo "$LYELLOW After result, to terminate the process, press 'CTRL+C'.$NORMAL"
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 
 # Запрос на ввод 
 read Target
