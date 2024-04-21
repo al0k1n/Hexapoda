705f1787dcd041b8842da802ae21cbc3cb868571
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 03:35:59 +0300

    Update sshversion.sh

--- sshversion.sh
+++ sshversion.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
 
 # Проверка наличия команды msfconsole
 # Eсли команда msfconsole не найдена, выводится сообщение об ошибке и
 # скрипт завершает свою работу с кодом выхода 1.
 if ! command -v msfconsole &> /dev/null; then
-    echo "Error: msfconsole is not installed. Please install it before using this script."
+    echo "$LRED Error: msfconsole is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
@@ -12,10 +21,10 @@ output_directory="outputs"
 output_file="sshversion_out.txt"
 
 # Команда для использования Metasploit для получения информации о SSH
-echo -n "Enter Target: "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 read Target
-echo "Exploiting SSH..."
+echo "$LYELLOW Exploiting SSH...$NORMAL"
 msfconsole -q -x "use auxiliary/scanner/ssh/ssh_version; set RHOSTS $Target;  run; exit" | tee "$output_directory/$output_file"
 echo "                                        "
-echo "Results have been saved to $output_file."
+echo "$LGREEN Results have been saved to $output_file.$NORMAL"
 echo "                                        "
