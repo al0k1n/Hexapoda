fcd08c4e76aa85f83114bb03eec1f9790988c0e3
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 03:33:52 +0300

    Update sshfingerprint.sh

--- sshfingerprint.sh
+++ sshfingerprint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-echo "After result, to terminate the process, press 'CTRL+C'."
-echo -n "[+] Enter Host: "
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+
+
+echo -e "$LYELLOW After result, to terminate the process, press 'CTRL+C'.$NORMAL"
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Host:$NORMAL "
 read Target
-echo "Find users..."
+echo -e "$LYELLOW Find users...$NORMAL"
 ssh root@$Target -v
