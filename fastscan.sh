739c682ccd1132173b57247f63c75ca6617a1ddf
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 03:07:21 +0300

    Update fastscan.sh

--- fastscan.sh
+++ fastscan.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
 # Цветом текста (жирным) (bold) :
-DEF='\033[0;39m'       #  ${DEF}
-DGRAY='\033[1;30m'     #  ${DGRAY}
-LRED='\033[1;31m'       #  ${LRED}
-LGREEN='\033[1;32m'     #  ${LGREEN}
-LYELLOW='\033[1;33m'     #  ${LYELLOW}
-LBLUE='\033[1;34m'     #  ${LBLUE}
-LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
-LCYAN='\033[1;36m'     #  ${LCYAN}
-WHITE='\033[1;37m'     #  ${WHITE}
-
-# Цветом текста (жирным) (bold) :
-DEF='\033[0;39m'       #  ${DEF}
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
 DGRAY='\033[1;30m'     #  ${DGRAY}
 LRED='\033[1;31m'       #  ${LRED}
 LGREEN='\033[1;32m'     #  ${LGREEN}
@@ -23,7 +11,7 @@ LCYAN='\033[1;36m'     #  ${LCYAN}
 WHITE='\033[1;37m'     #  ${WHITE}
 
 if ! command -v nmap &> /dev/null; then
-    echo "Error: nmap is not installed. Please install it before using this script."
+    echo "$LRED Error: nmap is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 # Переменная, которая содержит имя директории для сохранения результатов
@@ -32,14 +20,14 @@ output_directory="outputs"
 # Переменная, которая содержит имя файла для сохранения результатов
 output_file="fastscan_out.txt"
 
-echo -en "${LYELLOW} ${LGREEN}[+]${NORMAL}\n Enter Target : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter Target:$NORMAL "
 
 # Запрос на ввод
 read Target
-echo "Please wait..."
+echo -e "$LYELLOW Please wait...$NORMAL"
 
 # Команда, которая запускает утилиту nmap для сканирования 100 популярных TCP портов
 # Результат выводится на экран и записывается в директорию outputs с названием fastscan_out.txt
 nmap -F $Target | tee "$output_directory/$output_file"
 echo "                                        "
-echo -en "${LYELLOW} ${LGREEN}Results have been saved to $output_file.${NORMAL}\n"
+echo -en "$LGREEN Results have been saved to $output_file.${NORMAL}"
