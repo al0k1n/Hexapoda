8ed913ea96852f84ec609c61c533b914a528e163
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-04-21 04:22:00 +0300

    Update bruter.sh

--- bruter.sh
+++ bruter.sh
@@ -12,7 +12,7 @@ WHITE='\033[1;37m'     #  ${WHITE}
 green='\e[1;32m' # ${green}
 
 if ! command -v hydra &> /dev/null; then
-    echo "$LRED Hydra is not installed. Please install it before using this script.$NORMAL"
+    echo -e "$LRED Hydra is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
@@ -26,25 +26,29 @@ mkdir -p "$output_dir" || error_exit "Failed to create output directory: $output
 
 # Функция для вывода сообщения об ошибке и выхода из скрипта
 function error_exit {
-    echo "$LRED Error: $1$NORMAL"
+    echo -e "$LRED Error: $1$NORMAL"
     exit 1
 }
 
 # Запрос пути к файлам с логинами и паролями
-read -p "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter path to the login file: $NORMAL" login_file
-read -p "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter path to the password file: $NORMAL" password_file
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter path to the login file: $NORMAL"
+read login_file
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter path to the password file: $NORMAL" 
+read password_file
 
 # Проверка существования файлов с логинами и паролями
 [ -f "$login_file" ] || error_exit "Login file not found: $login_file"
 [ -f "$password_file" ] || error_exit "Password file not found: $password_file"
 
 # Запрос протокола и цели
-read -p "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the protocol $LYELLOW (ssh, ftp, telnet)$NORMAL$WHITE: $NORMAL" protocol
-read -p "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the target $LYELLOW (IP address)$NORMAL$WHITE: $NORMAL" target
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the protocol $LYELLOW (ssh, ftp, telnet)$NORMAL$WHITE: $NORMAL" 
+read protocol
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the target $LYELLOW (IP address)$NORMAL$WHITE: $NORMAL" 
+read target
 
 # Запуск Hydra для брутфорса
-echo "$LYELLOW Bruteforce starting...$NORMAL"
+echo -en "$LYELLOW Bruteforce starting...$NORMAL"
 hydra -f -L "$login_file" -P "$password_file" -o "$output_directory/$output_file" -e ns -v "$protocol://$target"
 echo "                                                        "
-echo "$LGREEN BruteForce End! Results have been saved to $output_file.$NORMAL"
+echo -en "$LGREEN BruteForce End! Results have been saved to $output_file.$NORMAL"
 echo "                                                        "
