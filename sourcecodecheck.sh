dd1432bdf585a0dbcd4f6695518b75191510a906
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-05-04 21:28:21 +0300

    Update sourcecodecheck.sh

--- sourcecodecheck.sh
+++ sourcecodecheck.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DEF='\033[0;39m'       #  ${DEF}
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+green='\e[1;32m' # ${green}
 
 output_directory="outputs"
 output_file="sourcecodecheck_out.txt"
 
-echo -n "[+] Enter Target : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the Target :$NORMAL "
 read Target
-echo "Please wait..."
+echo -e "$LYELLOW Please wait...$NORMAL"
 curl -i -L $Target | tee "$output_directory/$output_file"
 echo "                                        "
-echo "Results have been saved to $output_file."
+echo "$LGREEN Results have been saved to $output_file.$NORMAL"
 echo "                                        "
