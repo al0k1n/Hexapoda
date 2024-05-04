69beca5219673db2c9902a6133106a6ecf869ced
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-05-04 21:25:22 +0300

    Update scanudp100.sh

--- scanudp100.sh
+++ scanudp100.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
+NORMAL='\033[0m'      #  ${NORMAL}    # все атрибуты по умолчанию
+DEF='\033[0;39m'       #  ${DEF}
+DGRAY='\033[1;30m'     #  ${DGRAY}
+LRED='\033[1;31m'       #  ${LRED}
+LGREEN='\033[1;32m'     #  ${LGREEN}
+LYELLOW='\033[1;33m'     #  ${LYELLOW}
+LBLUE='\033[1;34m'     #  ${LBLUE}
+LMAGENTA='\033[1;35m'   #  ${LMAGENTA}
+LCYAN='\033[1;36m'     #  ${LCYAN}
+WHITE='\033[1;37m'     #  ${WHITE}
+green='\e[1;32m' # ${green}
 # Проверка наличия команды nmap
 # Eсли команда nmap не найдена, выводится сообщение об ошибке и
 # скрипт завершает свою работу с кодом выхода 1.
 if ! command -v nmap &> /dev/null; then
-    echo "Error: nmap is not installed. Please install it before using this script."
+    echo -e "$LRED Error: nmap is not installed. Please install it before using this script.$NORMAL"
     exit 1
 fi
 
@@ -14,16 +24,16 @@ output_directory="outputs"
 # Переменная, которая содержит имя файла для сохранения результатов
 output_file="scanudp100_out.txt"
 
-echo -n "[+] Enter Target : "
+echo -en "$LMAGENTA[$NORMAL$LCYAN+$LMAGENTA]$NORMAL$WHITE Enter the Target :$NORMAL "
 
 # Запрос на ввод
 read Target
 
-echo "Please wait..."
+echo -e "$LYELLOW Please wait...$NORMAL"
 
 # Команда, которая запускает утилиту nmap для сканирования 100 популярных UDP портов
 # Результат выводится на экран и записывается в директорию outputs с названием scanudp100_out.txt
 nmap -sU -sV --version-intensity 0 -n -F -T4 $target | tee "$output_directory/$output_file"
 echo "                                        "
-echo "Results have been saved to $output_file."
+echo "$LGREEN Results have been saved to $output_file.$NORMAL"
 echo "                                        "
