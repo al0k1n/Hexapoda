3b39451624188d396e16bf035712be21976ea27d
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-05-25 17:09:14 +0300

    Update ftpсheckvulns.sh

--- ftpсheckvulns.sh
+++ ftpсheckvulns.sh
@@ -30,7 +30,7 @@ read Target
 
 # После получения целевого хоста скрипт использует nmap с опцией --script ftp-* для запуска всех скриптов, относящихся к FTP.
 # Тем самым произойдёт проверка на наличие уязвимостей FTP на целевом хосте.
-nmap --script ftp-* -p21 $Target | tee "$output_directory/$output_file"
+nmap --script ftp-* $Target | tee "$output_directory/$output_file"
 echo "                                        "
 echo -e "$LGREEN Results have been saved to $output_file.$NORMAL"
 echo "                                        "
