df73fc500fb01062f142ea7e6c427f52fa99a1f2
al0k1n <91440074+al0k1n@users.noreply.github.com>
2024-05-26 14:55:24 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,9 +3,6 @@ function setupEnvironment(){
     echo "Сhecking your system and setting up the environment: "
     eval $(echo "$sudoo apt update;")    
 
-    echo "Installing golang"
-    eval $(echo "$sudoo apt install -y golang;")
-
     echo "Installing Python3/pip3"
     eval $(echo "$sudoo apt install -y python3;")
     eval $(echo "$sudoo apt install -y python3-pip;")
